e3b971bd40e4420c2eae651711e5645c84c13f77
Antony Kellermann <antony@aokellermann.dev>
2023-12-29 17:43:58 -0500

    Delete transaction category

--- api/Graph/src/Graph/GraphQL/Mutations/Transactions/Categories/CreateTransactionCategoryMutation.cs
+++ api/Graph/src/Graph/GraphQL/Mutations/Transactions/Categories/CreateTransactionCategoryMutation.cs
@@ -5,16 +5,14 @@ using Spend.Graph.Domain.Entities.Transactions;
 using Spend.Graph.Domain.Errors;
 using Spend.Graph.Infrastructure;
 
-namespace Spend.Graph.GraphQL.Mutations.Transactions;
+namespace Spend.Graph.GraphQL.Mutations.Transactions.Categories;
 
 [ExtendObjectType(OperationType.Mutation)]
-public class UpdateTransactionCategoryMutation
+public class CreateTransactionCategoryMutation
 {
     /// <summary>
-    ///     Update a transaction category.
+    ///     Creates a new transaction category.
     /// </summary>
-    /// <param name="id">Category id.</param>
-    /// <param name="version">Version.</param>
     /// <param name="name">Category name. 64 characters max.</param>
     /// <param name="description">Category description. 256 characters max.</param>
     /// <param name="parent">Parent id if any.</param>
@@ -25,8 +23,7 @@ public class UpdateTransactionCategoryMutation
     [Error(typeof(NotFoundException))]
     [Error(typeof(ConflictException))]
     [Error(typeof(PreconditionFailedException))]
-    public async Task<TransactionCategory> UpdateTransactionCategory(ObjectId id, long version, string name,
-        string? description, ObjectId? parent,
+    public async Task<TransactionCategory> CreateTransactionCategory(string name, string? description, ObjectId? parent,
         long? parentVersion, UserContext ctx, SpendDb db)
     {
         if (name.Length is 0 or > 64)
@@ -56,28 +53,30 @@ public class UpdateTransactionCategoryMutation
             }
         }
 
-        var withSameName = await (await db.TransactionCategories.FindAsync(
-            db.TransactionCategories.TenantFilter(ctx) &
-            Builders<TransactionCategory>.Filter.Eq(x => x.Name, name)
-        )).FirstOrDefaultAsync();
-        if (withSameName is not null && withSameName.Id != id)
+        if (await db.TransactionCategories.CountDocumentsAsync(
+                db.TransactionCategories.TenantFilter(ctx) &
+                Builders<TransactionCategory>.Filter.Eq(x => x.Name, name),
+                new CountOptions()
+                {
+                    Limit = 1
+                }) != 0)
         {
             throw new ConflictException(typeof(TransactionCategory), name);
         }
 
-        var res = await db.TransactionCategories.FindOneAndUpdateAsync(
-            db.TransactionCategories.AuditableFilter(id, version, ctx),
-            Builders<TransactionCategory>.Update
-                .Set(x => x.Name, name)
-                .Set(x => x.Description, description)
-                .Set(x => x.ParentTransactionCategoryId, parent)
-                .Audit(), new FindOneAndUpdateOptions<TransactionCategory>()
-            {
-                ReturnDocument = ReturnDocument.After
-            });
+        var transactionCategory = new TransactionCategory()
+        {
+            Name = name,
+            UserId = ctx.UserId!.Value,
+            Description = description,
+            ParentTransactionCategoryId = parent,
+            InsertedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow
+        };
+        await db.TransactionCategories.InsertOneAsync(transactionCategory);
 
         await sesh.CommitTransactionAsync();
 
-        return res;
+        return transactionCategory;
     }
 }
